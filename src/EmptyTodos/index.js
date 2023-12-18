3f2f67d4e82bb5d3770ee7a6d88e62980750cb22
brendasutara <sutarabrenda@gmail.com>
2023-12-18 19:26:19 -0300

    se agregó icono en componente emptyTodos

--- src/EmptyTodos/index.js
+++ src/EmptyTodos/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+/* import React from 'react';
 import './EmptyTodos.css';
 
 function EmptyTodos() {
   return (
-    <p>¡Crea tu primer to do!</p>
+    <div className='containerEmpty'>
+      <p className='EmptyTodos'>¡Crea tu primer to do!</p>
+    </div>
+  );
+}
+
+export { EmptyTodos }; */
+
+import React from 'react';
+import './EmptyTodos.css';
+import Icon from '../assets/flecha-down-right.svg'; // Ruta relativa al archivo SVG
+
+function EmptyTodos() {
+  return (
+    <div className='containerEmpty'>
+      <p className='EmptyTodos'>¡Crea tu primer to do!</p>
+      <img src={Icon} alt='Icon' className='Icon' />
+    </div>
   );
 }
 
 export { EmptyTodos };
+
